d5764f95ea6394dd20464d10c8797235627e2408
Robi <tiszarobi@gmail.com>
2021-03-07 13:19:09 +0100

    Upload

---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import Head from "next/head"
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit totam
+          libero nesciunt aliquam, repellendus quisquam et culpa est? A cum
+          inventore esse! Soluta, animi quisquam obcaecati minima quo nostrum
+          voluptates?
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
+          tenetur commodi eos recusandae soluta et omnis hic voluptas
+          exercitationem impedit, optio delectus quia! Quia obcaecati,
+          consequuntur quidem laborum alias animi.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
